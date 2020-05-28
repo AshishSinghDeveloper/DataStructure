a667b2b46d5c069458646e55b27b2768b6ad313b
ashishsng <ashishsng21@hotmail.com>
2020-05-27 22:14:57 -0500

    Adds Amazon Questions that comes on first round (may2020)

--- AmazonStack/AmazonQuestion1.cs
+++ AmazonStack/AmazonQuestion1.cs
@@ -1,10 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AmazonStack
 {
     public class AmazonQuestion1
     {
+        public int numberAmazonGoStores(int rows, int column, int[,] grid)
+        {
+            var sizes = new List<int>();
+            bool[,] visited = new bool[rows, column];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (grid[i, j] == 0)
+                        continue;
+                    sizes = TraverseNode(i, j, visited, grid, sizes);
+                }
+            }
+            return sizes.Count;
+
+        }
+        private List<int> TraverseNode(int i, int j, bool[,] visited, int[,] grid, List<int> sizes)
+        {
+            int currentRiverSize = 0;
+            var nodesToExplore = new List<Tuple<int, int>>();
+            var node = Tuple.Create(i, j);
+            nodesToExplore.Add(node);
+            while (nodesToExplore.Any()) //Returns true if list is not empty.
+            {
+                var currentNode = nodesToExplore.Last();
+                nodesToExplore.RemoveAt(nodesToExplore.Count - 1); //Pop - Remove last element from list;
+                i = currentNode.Item1; //similar to currentNode[0];
+                j = currentNode.Item2; //similar to currentNode[1];
+                if (visited[i, j])
+                    continue;
+                visited[i, j] = true;
+                if (grid[i, j] == 0)
+                    continue;
+                currentRiverSize += 1;
+                List<Tuple<int, int>> unvisitedNeighbors = GetUnVisitedNeighbors(i, j, grid, visited);
+                foreach (var neighbor in unvisitedNeighbors)
+                {
+                    nodesToExplore.Add(neighbor);
+                }
+            }
+            if (currentRiverSize > 0)
+            {
+                sizes.Add(currentRiverSize);
+            }
+            return sizes;
+        }
+        private List<Tuple<int, int>> GetUnVisitedNeighbors(int i, int j, int[,] grid, bool[,] visited)
+        {
+            var unvisitedNeighbors = new List<Tuple<int, int>>();
+            if (i > 0 && !visited[i - 1, j])
+            {
+                var top = Tuple.Create(i - 1, j);
+                unvisitedNeighbors.Add(top);
+            }
+            if (i < grid.GetLength(0) - 1 && !visited[i + 1, j])
+            {
+                var right = Tuple.Create(i + 1, j);
+                unvisitedNeighbors.Add(right);
+            }
+            if (j > 0 && visited[i, j - 1])
+            {
+                var left = Tuple.Create(i, j - 1);
+                unvisitedNeighbors.Add(left);
+            }
+            if (j < grid.GetLength(1) - 1 && !visited[i, j + 1])
+            {
+                var bottom = Tuple.Create(i, j + 1);
+                unvisitedNeighbors.Add(bottom);
+            }
+            return unvisitedNeighbors;
+        }
     }
 }
--- AmazonStack/AmazonQuestion2.cs
+++ AmazonStack/AmazonQuestion2.cs
@@ -6,5 +6,25 @@ namespace AmazonStack
 {
     public class AmazonQuestion2
     {
+        public List<string> reorderLines(int logFileSize, string[] logLines)
+        {
+            Array.Sort(logLines, (log1, log2) =>
+            {
+                string[] split1 = log1.Split(" ", 2);
+                string[] split2 = log2.Split(" ", 2);
+                bool isDigit1 = char.IsDigit(split1[1][0]);
+                bool isDigit2 = char.IsDigit(split2[1][0]);
+                if(!isDigit1 && !isDigit2)
+                {
+                    int cmp = split1[1].CompareTo(split2[1]);
+                    if (cmp != 0) return cmp;
+                    return split1[0].CompareTo(split2[0]);
+                }
+                return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
+            });
+            List<string> answer = new List<string>(logLines);
+            return answer;
+        }
+        // METHOD SIGNATURE ENDS
     }
 }
