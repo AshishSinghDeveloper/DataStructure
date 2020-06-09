3d8378943970d0c2390092a0e382403ce7f4c888
ashishsng <ashishsng21@hotmail.com>
2020-06-09 02:29:12 -0500

    Invert Binary tree using Queue

--- AmazonStack/InvertBinaryTree.cs
+++ AmazonStack/InvertBinaryTree.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Text;
 
 namespace AmazonStack
 {
     public class InvertBinaryTree
     {
+		//Time complexity: O(n) where n is number of node. We got this complexity because we are using while loop for each node
+		//Space Complexity: O(n) where n is number of node. We got this complexity because we are store all nodes in queue
         public void InvertBT(BinaryTree tree)
-        {
-			List<BinaryTree> Queue = new List<BinaryTree>();
-			int index = 0;
-			Queue.Add(tree);
-			while (index < Queue.Count)
-			{
-				BinaryTree current = Queue[index];
-				index += 1;
-
+		{ 	
+			//we are using BFS search here. You can guess this with Queue.
+			//In general, for implementing BFS we use Queue and for DFS we use Stack.
+			Queue<BinaryTree> queue = new Queue<BinaryTree>();
+			queue.Enqueue(tree);
+			while(queue.Count > 0)
+			{ 
+				BinaryTree current = queue.Dequeue();
 				if (current == null)
 				{
 					continue;
@@ -25,12 +27,11 @@ namespace AmazonStack
 
 				if (current.left != null)
 				{
-					Queue.Add(current.left);
+					queue.Enqueue(current.left);
 				}
-
 				if (current.right != null)
 				{
-					Queue.Add(current.right);
+					queue.Enqueue(current.right);
 				}
 			}
 		}
@@ -53,6 +54,17 @@ namespace AmazonStack
 			{
 				this.value = value;
 			}
+
+			public void insert(int[] num, int root)
+            {
+				
+                for (int i = 0; i < num.Length; i++)
+                {
+					BinaryTree newNode = new BinaryTree(num[i]);
+
+				}
+				
+            }
 		}
 
 
