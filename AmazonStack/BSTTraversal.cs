1862be0b2ae8f80daebaf57ab823e760b2837c68
AshishSinghDeveloper <ashishsng21@hotmail.com>
2020-07-05 18:42:14 -0500

    Adds question for BST Treversal

--- AmazonStack/BSTTraversal.cs
+++ AmazonStack/BSTTraversal.cs
@@ -1,5 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+//---------------------------------------------------------------------------------------------
+//Quesion: BST Traversal (AlgoExpert)
+
+//Write three functions that take in a Binary Search Tree (BST) and an empty array, traverse the BST,
+//    add its nodes'values to the input array, and return that array.
+//The three functions should traverse the BST using the in-order, pre-order, and post-order tree-traversal techniques, respectively.
+
+//Each BST node has an integer value, a left child node, and a right child node.
+//A node is said to be a valid BST node if and only if it satisfies the BST property:
+//  -its value is strictly greater than the values of every node to its left;
+//  -its value is less than or equal to the values of every node to its right;
+//  -and its children nodes are either valid BST nodes themselves or None / null.
+
+//Sample Input
+
+//            10
+//          /    \
+//         5      15
+//        / \    /   \
+//      2    5  13   22
+//     /          \
+//    1            14       
+//array = []
+
+//Sample output
+//inorder Traversal: [1, 2, 5, 5, 10, 13, 14, 22, 15]  // where the array is the input array
+//preOrder Traversal: [10, 5, 2, 1, 5, 15, 13, 14, 22] // where the array is the input array
+//postorder Traversal: [1, 2, 5, 5, 14, 13, 22, 15 10] // where the array is the input array
+//---------------------------------------------------------------------------------------------
 
 namespace AmazonStack
 {
