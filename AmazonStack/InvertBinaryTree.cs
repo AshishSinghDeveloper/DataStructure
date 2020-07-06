84c2311a9dcbbb5c6b6e13a5e98cfafbd55d210d
AshishSinghDeveloper <ashishsng21@hotmail.com>
2020-07-06 11:53:39 -0500

    Adds question for InvertBinaryTree

--- AmazonStack/InvertBinaryTree.cs
+++ AmazonStack/InvertBinaryTree.cs
@@ -1,7 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
-using System.Text;
+﻿using System.Collections.Generic;
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+//Question: Invert Binary Tree (AlgoExpert)
+//Write a function that takes in a Binary Tree and inverts it.
+//In other words, the function should swap every left node in the tree for its corresponding right node.
+
+//Each Binary Tree node has an Integer value, a left child node, and a night child node.
+//Children nodes can either be Binary Tree nodes themselves or None/null
+
+//Sample Input
+
+//tree =
+//            1
+//          /    \
+//         2      3
+//        / \    /   \
+//      4    5  6    7
+//     / \      
+//    8   9              
+//
+
+//Sample Output
+//            1
+//          /    \
+//         3      2
+//        / \    /   \
+//      7    6  5     4
+//                   / \      
+//                  8   9            
+//
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 
 namespace AmazonStack
 {
