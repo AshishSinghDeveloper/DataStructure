24145365d315c316775dd25a3944c045cab81e87
AshishSinghDeveloper <ashishsng21@hotmail.com>
2020-07-06 16:01:29 -0500

    Adds question for KCloestPointsToOrigin

--- AmazonStack/KClosestPointsToOrigin.cs
+++ AmazonStack/KClosestPointsToOrigin.cs
@@ -1,13 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Net.Http.Headers;
-using System.Text;
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+//Question: K Closest Points to Origin (Leetcode - Amazon Playlist)
+
+//We have a list of points on the plane. Find the K closest points to the origin(0, 0).
+//(Here, the distance between two points on a plane is the Euclidean distance.)
+//You may return the answer in any order.The answer is guaranteed to be unique (except for the order that it is in.)
+
+//Example 1
+//Input: points = [[1,3],[-2,2]], K = 1
+//Output: [[-2,2]]
+//Explanation: 
+//  The distance between(1, 3) and the origin is sqrt(10).
+//  The distance between(-2, 2) and the origin is sqrt(8).
+//  Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+//  We only want the closest K = 1 points from the origin, so the answer is just[[-2, 2]].
+
+//Example 2:
+//Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+//Output: [[3,3],[-2,4]]
+//(The answer [[-2, 4],[3,3]] would also be accepted.)
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 
 namespace AmazonStack
 {
     public class KClosestPointsToOrigin
     {
-        #region Approach 1 O(n)
+        #region Approach 1 O(nlogn)
         //public int[][] KClosest(int[][] points, int k)
         //{
         //    List<PointsAndDistane> list = new List<PointsAndDistane>();
@@ -39,7 +57,7 @@ namespace AmazonStack
                 dis[i] = DistanceFromOrigin(points[i]);
             }
             Array.Sort(dis);
-            int distk = dis[K - 1];
+            int distk = dis[K - 1]; //now it has lowest k-1 number
             int[][] answer = new int[K][];
             int t = 0;
 
