eb69fa13c7bc1113b0fccdb236b2e4287c0f8999
AshishSinghDeveloper <ashishsng21@hotmail.com>
2020-07-06 12:49:04 -0500

    Adds Question to Kadane's algo

--- AmazonStack/KadaneAlgo.cs
+++ AmazonStack/KadaneAlgo.cs
@@ -1,13 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+//Question: Kadane's algorithm (AlgoExpert)
+//Write a function that takes In a non-empty array of Integers and
+// returns the maximum sum that can be obtained by summing up all of the Integers in a non-empty subarray of the Input array,
+// A subarray must only contain adjacent numbers.
 
+//Sample Input
+//array = [3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]
+
+//Sample Output
+// 19
+
+//Explaination
+//[1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1] this has maximum adjacent sum
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 namespace AmazonStack
 {
-    public class KadaneAlgo
-    {
-  //      public int KadaneAlgorithm(int[] array)
-  //      {
+	public class KadaneAlgo
+	{
+		//  public int KadaneAlgorithm(int[] array)
+		//  {
 		//	if (array.Length == 0)
 		//	{
 		//		return 0;
@@ -63,30 +75,25 @@ namespace AmazonStack
 		//	return maxSum;
 		//}
 
-		private static int GetSum(int start, int end, int[] array)
-		{
-			int sum = 0;
-			for (int i = start; i <= end; i++)
-			{
-				sum += array[i];
-			}
-			return sum;
-		}
+		//private static int GetSum(int start, int end, int[] array)
+		//{
+		//	int sum = 0;
+		//	for (int i = start; i <= end; i++)
+		//	{
+		//		sum += array[i];
+		//	}
+		//	return sum;
+		//}
 		public int KadaneAlgorithm(int[] array)
 		{
-			List<string> val = new List<string>();
-			if (array.Length == 0) return 0;
 			int maxEndingHere = array[0];
 			int maxSoFar = array[0];
 			for (int i = 1; i < array.Length; i++)
 			{
 				maxEndingHere = Math.Max(array[i], maxEndingHere + array[i]);
 				maxSoFar = Math.Max(maxEndingHere, maxSoFar);
-				val.Add($"maxEndlingHere: {maxEndingHere} maxSoFar: {maxSoFar}");
 			}
 			return maxSoFar;
 		}
 	}
-
-	
 }
