4d98f1357b980ab32b0822f136511d10c70c03b1
AshishSinghDeveloper <ashishsng21@hotmail.com>
2020-07-07 14:00:18 -0500

    Adds question to Longest Substring without Repeating character

--- AmazonStack/LongestSubStringWithRepeatingCharacter.cs
+++ AmazonStack/LongestSubStringWithRepeatingCharacter.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+//Question: Longest Substring without repeating characters (Leetcode: Amazon Playlist)
+//Given a string, find the length of the longest substring without repeating characters.
+
+//Example 1:
+//Input: "abcabcbb"
+//Output: 3 
+//Explanation: The answer is "abc", with the length of 3.
+
+//Example 2:
+//Input: "bbbbb"
+//Output: 1
+//Explanation: The answer is "b", with the length of 1.
+
+//Example 3:
+//Input: "pwwkew"
+//Output: 3
+//Explanation: The answer is "wke", with the length of 3. 
+//             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+
 namespace AmazonStack
 {
     public class LongestSubStringWithRepeatingCharacter
     {
-        public LongestSubStringWithRepeatingCharacter()
-        {
-        }
-        public int SlidingWindow(String s)
+        //T: O(n) S: O(n), where n is length of string
+        public int LengthOfLongestSubstring(String s) //this is sliding window concept
         {
             int n = s.Length;
             HashSet<char> set = new HashSet<char>();
@@ -16,19 +35,20 @@ namespace AmazonStack
             while (i < n && j < n)
             {
                 // try to extend the range [i, j]
-                if (!set.Contains(s[j]))
+                if (!set.Contains(s[j])) //if string char doesn't exists in set
                 {
-                    set.Add(s[j++]);
-                    ans = Math.Max(ans, j - i);
+                    set.Add(s[j++]); //add string char and then increment j by 1.
+                    ans = Math.Max(ans, j - i); //take max. 
                 }
                 else
                 {
-                    set.Remove(s[i++]);
+                    set.Remove(s[i++]); //if string char does exists in set then remove it and increment i by 1.
                 }
             }
             return ans;
         }
-        public int LengthOfLongestSubstring(string s)
+        //T: O(n) S: O(n), where n is length of string
+        public int LengthOfLongestSubstring_Attempt1(string s)
         {
             int stringLength = s.Length;
             int answer = 0;
