efb1f6c6a1a004ba20e496b7306ca6995369452d
AshishSinghDeveloper <ashishsng21@hotmail.com>
2020-08-09 13:47:24 -0500

    Creates Continuous Binary Tree method

---
+++ AmazonStack/BinaryTree.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace AmazonStack
+{
+    public class BinaryTree
+    {
+        public int value;
+        public BinaryTree left;
+		public BinaryTree right;
+
+        public BinaryTree(int value)
+        {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+
+        public BinaryTree()
+        {
+
+        }
+        public void ContinuousInsert(int value)
+        {
+            BinaryTree current = this;
+            BinaryTree newNode = new BinaryTree(value);
+            Queue<BinaryTree> queue = new Queue<BinaryTree>();
+            queue.Enqueue(current);
+            while(queue != null)
+            {
+                var temp = queue.Dequeue();
+
+                if (temp.left == null)
+                {
+                    temp.left = newNode;
+                    break;
+                }
+                else
+                    queue.Enqueue(temp.left);
+
+                if (temp.right == null)
+                {
+                    temp.right = newNode;
+                    break;
+                }
+                else
+                    queue.Enqueue(temp.right);
+            }
+            
+        }
+        public BinaryTree SimpleBinaryTree()
+        {
+            BinaryTree binaryTree = new BinaryTree(1);
+            binaryTree.ContinuousInsert(2);
+            binaryTree.ContinuousInsert(3);
+            binaryTree.ContinuousInsert(4);
+            binaryTree.ContinuousInsert(5);
+            binaryTree.ContinuousInsert(6);
+            binaryTree.ContinuousInsert(7);
+            binaryTree.ContinuousInsert(8);
+            binaryTree.ContinuousInsert(9);
+            binaryTree.ContinuousInsert(10);
+            binaryTree.ContinuousInsert(11);
+
+
+            return binaryTree;
+        }
+    }
+    
+}
